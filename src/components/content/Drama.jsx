c339a62c48beba7a2e3f5ad85785b48f7b80a40f
unknown <sachinsiddhu112@gmail.com>
2024-10-31 17:50:12 +0530

    changed headline in content section

--- src/components/content/Drama.jsx
+++ src/components/content/Drama.jsx
@@ -5,7 +5,7 @@ export default function Drama() {
     <div className=' flex flex-col gap-3 w-full h-full px-5 mt-[90px]'>
       <div className='flex gap-3 items-end '>
         <span className='font-[900] text-[46px] text-green-600 opacity-30'>132,784</span>
-        <span className='font-[700] text-[30px] mb-[4px] text-blue-600'>Movies</span>
+        <span className='font-[700] text-[30px] mb-[4px] text-blue-600'>Drama</span>
       </div>
       <span className='text-[24px] font-[600] mb-10'>Most watched Dramas this month</span>
       <ContentCard/>
--- src/components/content/TVShows.jsx
+++ src/components/content/TVShows.jsx
@@ -5,7 +5,7 @@ export default function TVShows() {
     <div className=' flex flex-col gap-3 w-full h-full px-5 mt-[90px]'>
       <div className='flex gap-3 items-end '>
         <span className='font-[900] text-[46px] text-red-400 opacity-30'>132,784</span>
-        <span className='font-[700] text-[30px] mb-[4px] text-blue-600'>Movies</span>
+        <span className='font-[700] text-[30px] mb-[4px] text-blue-600'>Tv Shows</span>
       </div>
       <span className='text-[24px] font-[600] mb-10'>Most watched TV shows this month</span>
       <ContentCard/>
